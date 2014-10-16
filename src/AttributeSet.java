0ee378b28e6706dad166ebe4c94487b75737ed1f
anhiancheong <Andrew@Andrew-VAIO.(none)>
2014-10-15 21:08:33 -0400

    next version of jar

--- src/AttributeSet.java
+++ src/AttributeSet.java
@@ -63,7 +63,7 @@ public class AttributeSet {
 		for(Attribute curAttr: valInstances) {
 			double tempProb = findProb(curAttr);
 			debugPrint.print("Probablility of attribute: " + curAttr.getName() + " - " + curAttr.getVal() + " is: " + tempProb);
-			if(tempProb > threshold && seen.containsKey(curAttr.hashCode())) {
+			if(tempProb > threshold && !seen.containsKey(curAttr.hashCode())) {
 				debugPrint.print("Found value above the threshold");
 				retAttr.add(new Attribute(curAttr.getName(), curAttr.getVal(), tempProb, getAllSources(curAttr)));
 				seen.put(curAttr.hashCode(), true);
