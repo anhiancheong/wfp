6d98ad0a0d1281e0b8d83c43629464c7de105c0c
anhiancheong <Andrew@Andrew-VAIO.(none)>
2014-10-17 20:50:24 -0400

    next version of jar

--- src/driver.java
+++ src/driver.java
@@ -26,7 +26,7 @@ public class driver {
         //read in config file
         loadConfig("testConfig.txt");
         //initialize person
-        Person experimentPerson = new Person("amy", "clark", websitesUsed);
+        Person experimentPerson = new Person("amy", "allen", websitesUsed);
         //populate initial values of person
         if(!experimentPerson.populateInitialAttributes(initialAttributeNames)){
         	debugPrint.print("Could not the sufficient ground truth values for this person");
