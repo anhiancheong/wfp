af1f94e1f2602b2fdb7ccbb732422506a162200b
anhiancheong <Andrew@Andrew-VAIO.(none)>
2014-10-20 20:05:11 -0400

    next version of jar

--- src/dbWrapper.java
+++ src/dbWrapper.java
@@ -199,7 +199,7 @@ public class dbWrapper {
 	public String getGtId(String firstName, String lastName) {
 		
 		String gtId = "";
-		currentQuery = "SELECT gt_id FROM gt_person WHERE lower(first_name_ = lower('" + firstName + "') AND lower(last_name) = lower('" + lastName + "');";		
+		currentQuery = "SELECT gt_id FROM gt_person WHERE lower(first_name) = lower('" + firstName + "') AND lower(last_name) = lower('" + lastName + "');";		
 		execute();
 		
 		try {
